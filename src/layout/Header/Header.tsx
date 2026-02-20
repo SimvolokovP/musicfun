68433714867803827dc36649daa182631822fa94
Simvolokov Pavel <psimvolokov@gmail.com>
2026-02-20 14:08:02 +0300

    feat: add logo

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,11 @@
-import { LanguageSwitcher } from "../../shared/components/LanguageSwitcher/LanguageSwitcher";
+import { LanguageSwitcher } from "@/shared/components/LanguageSwitcher/LanguageSwitcher";
+import { Logo } from "@/shared/ui/Logo/Logo";
 
 export const Header = () => {
   return (
     <header className="py-2 h-16 md:h-20 bg-bg-primary w-full">
       <div className="container flex items-center justify-between h-full">
-        <div>Musicfun</div>
+        <Logo />
         <div className="flex gap-5 items-center">
           <LanguageSwitcher />
           <button>2</button>
